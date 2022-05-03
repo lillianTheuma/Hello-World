62db7a1b15d6821e2afe32c4494591e959ebafe2
Lillian Theuma <definatelyruby@gmail.com>
2022-05-02 22:26:52 -0700

    Added features for Hello World class

--- edu/pcc/cis/helloworld/HelloWorld.java
+++ edu/pcc/cis/helloworld/HelloWorld.java
@@ -1,5 +1,10 @@
 package edu.pcc.cis.helloworld;
 
 public class HelloWorld {
-    
+    public void sayHello() {
+        System.out.println("Hello, world!");
+    }
+    public void sayHello(String name) {
+        System.out.println("Hello, " + name);
+    }
 }
